35c6b7971c5cc22e89dc8aeaaa047e99ee973002
MansourWolou <Wolou0444@gmail.com>
2023-02-07 04:15:37 +0100

    structure des routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const config = require("./config");
 
+const retailerRouter = require("./retailer/retailer.route").router;
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -11,10 +13,15 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
+    response.json({ info: 'Node.js, Express, and Mysql API' })
 })
-
  
+app.use("/api/v1/retailer", retailerRouter);
+//TODO:
+/**
+ * app.use("/api/v1/customer", customerRouter);
+ * app.use("/api/v1/admin", adminRouter);
+ */
 app.listen(config.PORT, () => {
     console.log(`Application listening on:http://localhost:${config.PORT}`);
   });
