144a7b64948c1fc9fa674a4f873bc9c830687f30
fmaxime <maxime.fuzeau@ynov.com>
2023-02-07 11:32:16 +0100

    :construction:

--- front/src/components/Auth/registerPage.vue
+++ front/src/components/Auth/registerPage.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="q-pa-xl">
-      <div class="flex flex-center column">
-        <h1>Inscrivez-vous</h1>
-        <q-card>
-            <q-card-section>
-                <q-input label="Email" type="email" outlined class="q-mb-md" v-model="form.email" />
-                <q-input label="Mot de passe" type="password" outlined class="q-mb-md" v-model="form.password" :rules="[ val => val.length >= 6 || 'Minimum 6 caractère']" />
-                <q-checkbox label="terms and conditions" v-model="form.terms_and_conditions"/>
-                <q-btn label="S'incrire" class="full-width" color="primary" @click="handleRegister"/>
-                <p>Vous avez déjà un compte ? <Router-link to="/login">Connectez-vous</Router-link></p>
-            </q-card-section>
-        </q-card>
-      </div>
+  <div class="q-pa-xl">
+    <div class="flex flex-center column">
+      <h1>Inscrivez-vous</h1>
+      <q-card>
+          <q-card-section>
+              <q-input label="Email" type="email" outlined class="q-mb-md" />
+              <q-input label="Mot de passe" type="password" outlined class="q-mb-md" :rules="[ val => val.length >= 6 || 'Minimum 6 caractère']" />
+              <q-checkbox label="terms and conditions" />
+              <q-btn label="S'incrire" class="full-width" color="primary"/>
+              <p>Vous avez déjà un compte ? <Router-link to="/login">Connectez-vous</Router-link></p>
+          </q-card-section>
+      </q-card>
     </div>
+  </div>
 </template>
 <script setup>
 import { Notify } from 'quasar'
@@ -24,17 +24,17 @@ const userStore = useUserStore()
 const router = useRouter()
 
 const form = ref({
-  email: '',
-  password: '',
-  terms_and_conditions: false
+email: '',
+password: '',
+terms_and_conditions: false
 })
 
 const handleRegister = async () => {
-  try {
-    await userStore.handleRegister(form.value)
-    router.push({ name: 'dashboard' })
-  } catch (e) {
-    Notify.create('Error during register')
-  }
+try {
+  await userStore.handleRegister(form.value)
+  router.push({ name: 'dashboard' })
+} catch (e) {
+  Notify.create('Error during register')
+}
 }
 </script>
